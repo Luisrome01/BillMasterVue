4fbe31ca02fef02289ec53be83ae6c7c22b540d4
alejandroamesty <alejandro.30337890@uru.edu>
2024-03-31 15:52:14 -0400

    Crear componente NavBar

---
+++ src/components/navBar/navBar.vue
@@ -0,0 +1,176 @@
+<template>
+    <nav class="NavNavbar">
+        <div class="NavLogo">
+            <img :src="logo" alt="logo">
+            <h1 class="NavBarTitle">Bill Master</h1>
+        </div>
+        <div class="NavContent">
+            <h2 style="color: #A4A4A5;">MENU PRINCIPAL</h2>
+            <div class="nav-link" @click="setComponenteActivo('Productos')" :style="getStyle('Productos')"
+                @mouseenter="handleMouseEnter('Productos')" @mouseleave="handleMouseLeave('Productos')">
+                <!-- SVG y Texto para Productos aquí -->
+                <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    class="nav-link-img">
+                    <!-- Tus paths aquí -->
+                </svg>
+                <h3 :style="{ color: componenteActivo === 'Productos' ? activeLogoColor : baseLogoColor }">Productos
+                </h3>
+            </div>
+            <div class="nav-link" @click="setComponenteActivo('Facturación')" :style="getStyle('Facturación')"
+                @mouseenter="handleMouseEnter('Facturación')" @mouseleave="handleMouseLeave('Facturación')">
+                <!-- SVG y Texto para Productos aquí -->
+                <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    class="nav-link-img">
+                    <!-- Tus paths aquí -->
+                </svg>
+                <h3 :style="{ color: componenteActivo === 'Facturación' ? activeLogoColor : baseLogoColor }">Facturación
+                </h3>
+            </div>
+            <div class="nav-link" @click="setComponenteActivo('Métodos de Pago')" :style="getStyle('Métodos de Pago')"
+                @mouseenter="handleMouseEnter('Métodos de Pago')" @mouseleave="handleMouseLeave('Métodos de Pago')">
+                <!-- SVG y Texto para Métodos de Pago aquí -->
+                <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    class="nav-link-img">
+                    <!-- Tus paths aquí -->
+                </svg>
+                <h3 :style="{ color: componenteActivo === 'Métodos de Pago' ? activeLogoColor : baseLogoColor }">Métodos de Pago
+                </h3>
+            </div>
+            <div class="nav-link" @click="setComponenteActivo('Cierre de Caja')" :style="getStyle('Cierre de Caja')"
+                @mouseenter="handleMouseEnter('Cierre de Caja')" @mouseleave="handleMouseLeave('Cierre de Caja')">
+                <!-- SVG y Texto para Cierre de Caja aquí -->
+                <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    class="nav-link-img">
+                    <!-- Tus paths aquí -->
+                </svg>
+                <h3 :style="{ color: componenteActivo === 'Cierre de Caja' ? activeLogoColor : baseLogoColor }">Cierre de Caja
+                </h3>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+
+export default {
+    props: ['componenteActivo', 'setComponenteActivo'],
+    setup(props) {
+        const baseLogoColor = "#A4A4A5";
+        const activeLogoColor = "#000000";
+        const logo = import('../../assets/BillMasterLogo.svg'); // Asegúrate de que webpack pueda manejar esto, o usa un import estático en la parte superior.
+
+        const componenteHover = reactive({
+            Productos: false,
+            Facturacion: false,
+            "Metodos de Pago": false,
+            "Cierre de Caja": false,
+        });
+
+        function handleMouseEnter(componente) {
+            componenteHover[componente] = true;
+        }
+
+        function handleMouseLeave(componente) {
+            componenteHover[componente] = false;
+        }
+
+        function getStyle(componente) {
+            if (props.componenteActivo === componente) {
+                return {
+                    backgroundColor: "#AEBBFD",
+                    boxShadow: "5px 5px 1px rgba(0, 0, 0, 1)",
+                };
+            } else if (componenteHover[componente]) {
+                return {
+                    backgroundColor: "lightGray",
+                };
+            }
+            return {};
+        }
+
+        return {
+            baseLogoColor,
+            activeLogoColor,
+            logo,
+            handleMouseEnter,
+            handleMouseLeave,
+            getStyle,
+        };
+    },
+};
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+}
+
+.NavNavbar {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.NavLogo {
+    flex-basis: 10%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    gap: 20px;
+    border-bottom: 2px solid #ebebeb;
+}
+
+.NavContent {
+    margin-top: 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-basis: 80%;
+    gap: 40px;
+    position: relative;
+}
+
+.nav-links {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+}
+
+.nav-link-img {
+    width: 30px;
+    height: 30px;
+    margin-left: 15px;
+    margin-right: 15px;
+}
+
+.nav-link {
+    cursor: pointer;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 50px;
+    border-radius: 10px;
+    width: 80%;
+    transition: 0.5s;
+}
+
+.nav-last-link {
+    margin-top: auto;
+    margin-left: 50px;
+    margin-bottom: 50px;
+    cursor: pointer;
+}
+
+.NavBarTitle {
+    color: black;
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,12 +1,141 @@
 <template>
     <div>
-        <h1>Main</h1>
+        <div className="MainContainer">
+            <div className="MainNavContainer">
+                <NavBar
+                    componenteActivo={componenteActivo}
+                    setComponenteActivo={setComponenteActivo}
+                />
+            </div>
+            <div className="MainContentContainer">
+                <div className="MainContentTop">
+                    <h1 className="MainTitle">{componenteActivo}</h1>
+
+                    <div className="MainUserDiv">
+                        <img></img>
+                        <p>
+                            Bienvenido {getUser}
+                        </p>
+                    </div>
+                </div>
+                <div className="FactContentBottom">
+                    {componenteActivo === "Productos" && (
+                        <Productos
+                            setListaProductos={setListProductos}
+                            listaProductos={listProductos}
+                        />
+                    )}
+                    {componenteActivo === "Facturacion" && (
+                        <Facturacion
+                            listaProductosInterna={listProductos}
+                            setListaProductosExterna={setListProductos}
+                            continuarVista={cambiarMetodoPago}
+                            ClienteExterno={cliente}
+                            setClienteExterno={setCliente}
+                        />
+                    )}
+                    {componenteActivo === "Metodos de Pago" && (
+                        <MetodosPago totalCosto={totalFactura.toFixed(2)} />
+                    )}
+                    {componenteActivo === "Cierre de Caja" && (
+                        <CierreCaja responsable={getUser} />
+                    )}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import NavBar from '../components/navBar/navBar.vue';
+
 export default {
-}
+    components: {
+        NavBar,
+    },
+};
 </script>
 
-<style></style>
+<style>
+body {
+    background-color: #ffffff;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    box-sizing: border-box;
+    color: black;
+}
+
+html {
+    height: 100%;
+}
+
+.MainContainer {
+    display: flex;
+    flex-direction: row;
+    background-color: #ffffff;
+    width: 100%;
+    height: 100%;
+}
+
+.MainNavContainer {
+    flex-basis: 22.5%;
+    border-right: 2px solid #ebebeb;
+}
+
+.MainContentContainer {
+    flex-basis: 77.5%;
+    display: flex;
+    flex-direction: column;
+    width: 75%;
+    height: 100vh;
+    border-radius: 20px;
+    background-color: #f5f5f5;
+}
+
+.MainContentTop {
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 25px;
+    flex-basis: 15%;
+    position: relative;
+}
+
+.MainContentTop::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 5%;
+    width: 90%;
+    height: 5px;
+    background-image: linear-gradient(to right, #aebbfd, #ff6060);
+    background-repeat: no-repeat;
+    border-radius: 5px;
+}
+
+.MainContentBottom {
+    flex-basis: 85%;
+    background-color: #ffffff;
+}
+
+.MainTitle {
+    color: black;
+    font-weight: bold;
+    flex-basis: 70%;
+    margin-left: 5%;
+}
+
+.MainUserDiv {
+    flex-basis: 20%;
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    justify-items: center;
+    align-items: center;
+    height: 45px;
+    background-color: #aebbfd;
+    border-radius: 10px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 1);
+}
+</style>
