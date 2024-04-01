41f617375deecb797961d663f88ea7d5568dc928
Luis Romero <tatoxpro01@gmail.com>
2024-03-31 22:19:12 -0400

    Se empezo la pantalla Metodos de pago, falta

---
+++ src/components/inputs/InputMetodosPago.vue
@@ -0,0 +1,108 @@
+<template>
+    <div :style="containerStyle" class="InputMetodoPagoContainer">
+      <div v-for="(item, index) in inputs" :key="index" class="IDIFMainContainer" :style="{ height: height }">
+        <p class="IDIFTitle" :style="titleStyle">{{ item.label }}</p>
+        <select v-model="item.value" @change="handleChange(index)" class="IDIFSelect" :disabled="shouldDisableSelect(index)">
+          <option :value="item.placeholder" hidden>{{ item.placeholder }}</option>
+          <option v-for="option in item.options" :key="option.value" :value="option.value">{{ option.label }}</option>
+        </select>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      width: String,
+      height: String,
+      color: String,
+      padding: String,
+      borderRadius: String,
+      valorMetodoPago: Function,
+      valorBanco: Function
+    },
+    data() {
+      return {
+        inputs: [
+          { label: "Metodo de Pago:", value: "", placeholder: "Metodo de Pago:", options: [
+            { label: "EFECTIVO", value: "EFECTIVO" },
+            { label: "TRANSFERENCIA", value: "TRANSFERENCIA" },
+            { label: "DIVISAS", value: "DIVISAS" },
+            { label: "TARJETA", value: "TARJETA" }
+          ] },
+          { label: "Banco:", value: "", placeholder: "Banco:", options: [
+            { label: "BANESCO", value: "BANESCO" },
+            { label: "MERCANTIL", value: "MERCANTIL" },
+            { label: "BANCARIBE", value: "BANCARIBE" }
+          ] }
+        ]
+      };
+    },
+    computed: {
+      containerStyle() {
+        return {
+          width: this.width,
+          height: this.height,
+          backgroundColor: this.color,
+          padding: this.padding,
+          borderRadius: this.borderRadius,
+          textAlign: "center"
+        };
+      },
+      titleStyle() {
+        return {
+          fontWeight: "bold"
+        };
+      }
+    },
+    methods: {
+      handleChange(index) {
+        const item = this.inputs[index];
+        if (index === 0) {
+          this.valorMetodoPago(item.value);
+          if (item.value !== "TRANSFERENCIA" && item.value !== "TARJETA") {
+            this.inputs[1].value = ""; // Limpiar el valor del banco cuando no se selecciona transferencia o tarjeta
+          }
+        } else {
+          this.valorBanco(item.value);
+        }
+      },
+      shouldDisableSelect(index) {
+        // Si el método de pago seleccionado es "TRANSFERENCIA" o "TARJETA", se debe habilitar el select del banco
+        return index === 1 && (this.inputs[0].value !== "TRANSFERENCIA" && this.inputs[0].value !== "TARJETA");
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .IDIFMainContainer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  
+  .IDIFSelect {
+    margin-left: 10px;
+    color: black;
+    width: auto; 
+    height: 28px; 
+    border: 2px solid black;
+    border-radius: 3px;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .IDIFTitle {
+    font-weight: bold;
+  }
+  
+  .InputMetodoPagoContainer {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+  }
+  </style>
+  
--- src/components/navBar/views/MetodoPago.vue
+++ src/components/navBar/views/MetodoPago.vue
@@ -1,9 +1,128 @@
 <template>
-    <h1>Aquí estará la vista de Métodos de Pago</h1>
-</template>
+    
+    <div class="MetodosContainer">
+				<h1 class="MetodosHeaderContainer">Agregar metodos de pago</h1>
+				<div class="MetodosInput">
+					<div class="MetodoMetododPago">
+                        <inputMetodosPago 
+                        :name="'Metodo de pago:'"
+                        color="#D9D9D9"
+                        width="150px"
+                        padding="5px"
+                        borderRadius="10px"
+                        height="20%"
+                        :valorMetodoPago="setMetodoPago"
+                        :valorBanco="setBanco"/>
+                    </div>
+					<div class="MetodoMonto-BotonAgregar">
+						<div class="MetodoMonto">
+							<InputDiferente name="Monto:" color="#D9D9D9" width="15%" onChange={setMonto} value={monto} />
+						</div>
+						<div class="MetodoBotonAgregar">
+							<BtnGeneral :img="svgAdd" text="Agregar Pago" width="165px" />
+						</div>
+					</div>
+				</div>
 
-<style>
-h1 {
-    color: black;
+				<div class="MetodosPagoTableContainer">
+					
+				</div>
+
+				<div class="MetodosCheckoutContainer">
+					
+					<BtnGeneral text="Checkout"
+						width="140px"
+						color="#ff6060"
+						onHoverColor="#c54444"
+						img={marketCartSVG} />
+					
+				</div>
+			</div>
+  </template>
+  
+  <script>
+import InputDiferente from '../../inputs/InputDiferente.vue';
+import InputMetodosPago from '../../inputs/InputMetodosPago.vue'; 
+import BtnGeneral from '../../buttons/BtnGeneral.vue'; 
+
+
+  export default {
+    components: {
+      InputMetodosPago, InputDiferente, BtnGeneral
+    },
+    methods: {
+      handleValorMetodoPago(valor) {
+        console.log('Valor del método de pago seleccionado:', valor);
+        // Aquí puedes realizar cualquier acción necesaria con el valor del método de pago seleccionado
+      },
+      handleValorBanco(valor) {
+        console.log('Valor del banco seleccionado:', valor);
+        // Aquí puedes realizar cualquier acción necesaria con el valor del banco seleccionado
+      }
+    }
+  };
+
+  
+  </script>
+  
+  <style scoped>
+  .MetodosContainer {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 85vh;
+	background-color: #ffff;
+	position: relative;
 }
-</style>
+
+.MetodosHeaderContainer {
+	flex-basis: 10%;
+	margin-left: 7%;
+	align-items: center;
+	display: flex;
+}
+
+.MetodosInput {
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+   
+}
+
+.MetodoMetododPago {
+	margin-left: 10%;
+	align-items: center;
+	display: flex;
+	flex-direction: row;
+	gap: 25px;
+}
+
+.MetodoMonto-BotonAgregar {
+	display: flex;
+	flex-direction: column;
+	margin-left: 10%;
+	gap: 20px;
+}
+
+.MetodosPagoTableContainer {
+	width: 60%;
+	display: flex;
+	align-self: center;
+	justify-content: center;
+	flex-basis: 50%;
+}
+
+.MetodosCheckoutContainer {
+	align-self: normal;
+	flex-basis: 15%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 30px;
+	margin-left: auto;
+	margin-right: 10%;
+}
+
+
+  </style>
+  
