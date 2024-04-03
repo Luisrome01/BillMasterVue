eb4ebf9d3a230b59030850122d5350b7c042e82c
Tryzack <joseandres102003@gmail.com>
2024-04-02 20:29:27 -0400

    add Funcionalidad Facturacion

--- src/components/navBar/views/Facturacion.vue
+++ src/components/navBar/views/Facturacion.vue
@@ -1,78 +1,115 @@
 <template>
-  <div class="FacturaContainer">
-    <h1 class="FacturaHeaderContainer">Nueva Factura</h1>
+	<div class="FacturaContainer">
+		<h1 class="FacturaHeaderContainer">Nueva Factura</h1>
 
-    <div class="FacturaInputsEntre2">
-      <div class="FacturaInput1">
-        <div class="FacturaCedula-nombre">
-          <div class="FacturaCedula">
-            <InputDinamico :value="getValorIdentificacion" name="Cedula o Pasaporte:" color="#D9D9D9" width="200px"
-              @blur="handleOnBlur" @onTypeChange="newType => setIdentificacion(newType)"
-              @onValueChange="newValue => setValorIdentificacion(newValue)" />
-          </div>
+		<div class="FacturaInputsEntre2">
+			<div class="FacturaInput1">
+				<div class="FacturaCedula-nombre">
+					<div class="FacturaCedula">
+						<InputDinamico
+							:value="getValorIdentificacion"
+							name="Cedula o Pasaporte:"
+							color="#D9D9D9"
+							width="200px"
+							@blur="handleOnBlur"
+							@onTypeChange="(newType) => setIdentificacion(newType)"
+							@onValueChange="(newValue) => setValorIdentificacion(newValue)"
+						/>
+					</div>
 
-          <div class="FacturaNombre">
-            <InputDiferente :value="getValorNombre" name="Nombre:" color="#D9D9D9" width="50%"
-              placeholder="ej. Jhon Doe" @change="newName => setName(newName)" :disabled="disabledInput" />
-          </div>
-        </div>
-        <div class="FacturaDireccion">
-          <InputDiferente name="Direccion:" :value="getValorDireccion" flexBasis="100%" color="#D9D9D9" width="75vh"
-            placeholder="ej. Avenida Río Caura Torre Humboldt Prados del Este Piso 20 Oficina 20-06"
-            :disabled="disabledInput" @change="newDireccion => setDireccion(newDireccion)" />
-        </div>
-        <div class="FacturaRif-BotonCrear">
-          <div class="FacturaRif">
-            <InputDiferente :value="getValorRif" name="Rif:" color="#D9D9D9" width="80%" placeholder="ej. J123456789"
-              :disabled="disabledInput" @change="newRif => setRif(newRif)" />
-          </div>
-          <div class="FacturaBotonCrear">
-            <BtnGeneral :img="svgAdd" text="Crear Cliente" width="165px" @click="createClient" />
-          </div>
-        </div>
-      </div>
+					<div class="FacturaNombre">
+						<InputDiferente
+							:value="getValorNombre"
+							name="Nombre:"
+							color="#D9D9D9"
+							width="50%"
+							placeholder="ej. Jhon Doe"
+							@change="(newName) => setName(newName)"
+							:disabled="disabledInput"
+						/>
+					</div>
+				</div>
+				<div class="FacturaDireccion">
+					<InputDiferente
+						name="Direccion:"
+						:value="getValorDireccion"
+						flexBasis="100%"
+						color="#D9D9D9"
+						width="70%"
+						placeholder="ej. Avenida Río Caura Torre Humboldt Prados del Este Piso 20 Oficina 20-06"
+						:disabled="disabledInput"
+						@change="(newDireccion) => setDireccion(newDireccion)"
+					/>
+				</div>
+				<div class="FacturaRif-BotonCrear">
+					<div class="FacturaRif">
+						<InputDiferente
+							:value="getValorRif"
+							name="Rif:"
+							color="#D9D9D9"
+							width="80%"
+							placeholder="ej. J123456789"
+							:disabled="disabledInput"
+							@change="(newRif) => setRif(newRif)"
+						/>
+					</div>
+					<div class="FacturaBotonCrear">
+						<BtnGeneral :img="svgAdd" text="Crear Cliente" width="165px" @click="createClient" />
+					</div>
+				</div>
+			</div>
 
-      <div class="FacturaInput2">
-        <div class="FacturaoCodigo-buscar">
-          <div class="FacturaCodigo">
-            <InputDiferente :value="getValorCodigo" name="Codigo:" color="#D9D9D9" @change="newValue => {
-              setCodigo(newValue);
-              setValorCodigo(newValue);
-            }" />
-          </div>
-          <div class="FacturaBuscar">
-            <button class="FacturaSearch">
-              <img :src="svgSearch" alt="Buscar">
-            </button>
-          </div>
-        </div>
-        <div class="FacturaCantidad">
-          <InputDiferente :value="getValorCantidad" type="number" name="Cantidad:" color="#D9D9D9" width="80px"
-            placeholder="1" @change="newCantidad => {
-              setCantidad(newCantidad);
-              setValorCantidad(newCantidad);
-            }" />
-        </div>
-        <div class="FacturaBotonAgregar">
-          <BtnGeneral :img="svgAdd" text="Agregar Producto" width="200px" @click="addProduct" />
-        </div>
-      </div>
-    </div>
+			<div class="FacturaInput2">
+				<div class="FacturaoCodigo-buscar">
+					<div class="FacturaCodigo">
+						<InputDiferente
+							:value="getValorCodigo"
+							name="Codigo:"
+							color="#D9D9D9"
+							@change="
+								(newValue) => {
+									/* setCodigo(newValue);
+              setValorCodigo(newValue); */
+								}
+							"
+							ref="inputCodigo"
+						/>
+					</div>
+					<div class="FacturaBuscar">
+						<button class="FacturaSearch">
+							<img :src="svgSearch" alt="Buscar" />
+						</button>
+					</div>
+				</div>
+				<div class="FacturaCantidad">
+					<InputDiferente
+						:value="getValorCantidad"
+						type="number"
+						name="Cantidad:"
+						color="#D9D9D9"
+						width="80px"
+						placeholder="1"
+						ref="inputCantidad"
+					/>
+				</div>
+				<div class="FacturaBotonAgregar">
+					<BtnGeneral :img="svgAdd" text="Agregar Producto" width="200px" @click="addProduct" />
+				</div>
+			</div>
+		</div>
 
-    <div class="FacturaTableContainer">
-      <ProductTable width="90%" height="85%" :rows="listProductos" @eliminarProducto="eliminarProducto" />
-    </div>
+		<div class="FacturaTableContainer">
+			<ProductTable width="90%" height="85%" :rows="listProductos" @eliminarProducto="removeProduct" />
+		</div>
 
-    <div class="FacturaCheckoutContainer">
-      <div style="display: flex; flex-direction: column;">
-        <p style="position: relative; margin-left: auto; font-size: 18px;">Total:</p>
-        <p style="position: relative; margin-left: auto; font-size: 25.4331px; font-weight: bold;">$ {{ montoTotal }}
-        </p>
-      </div>
-      <BtnGeneral text="Metodo de Pago" width="150px" color="#ff6060" :img="cartSVG" @click="continueToPayment"
-        :class="{ 'hover-class': hovered }" />
-    </div>
-  </div>
+		<div class="FacturaCheckoutContainer">
+			<div style="display: flex; flex-direction: column">
+				<p style="position: relative; margin-left: auto; font-size: 18px">Total:</p>
+				<p style="position: relative; margin-left: auto; font-size: 25.4331px; font-weight: bold">$ {{ montoTotal }}</p>
+			</div>
+			<BtnGeneral text="Metodo de Pago" width="150px" color="#ff6060" :img="cartSVG" @click="continueToPayment" />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -86,231 +123,264 @@ import svgSearch from "../../../assets/SearchSVG.svg";
 import cartSVG from "../../../assets/marketKart.svg";
 
 export default {
-  name: "Facturacion",
-  components: {
-    InputDinamico,
-    InputDiferente,
-    BtnGeneral,
-    ProductTable,
-  },
-  props: {
-    setListaProductosExterna: Function,
-    continuarVista: Function,
-    listaProductosInterna: Array,
-    setClienteExterno: Function,
-    ClienteExterno: Object,
-  },
-  setup(props) {
-    const listProductos = ref(props.listaProductosInterna || []);
-    const montoTotal = ref("0.00");
-    const getCantidad = ref(1);
-    const getCodigo = ref("");
-    const getIdentificacion = ref("Cedula");
-    const getValorIdentificacion = ref(
-      props.ClienteExterno
-        ? props.ClienteExterno.ci || props.ClienteExterno.pasaporte || props.ClienteExterno.idExtranjera
-        : ""
-    );
-    const getClientes = ref([]);
-    const getValorNombre = ref(props.ClienteExterno ? props.ClienteExterno.name : "");
-    const getValorDireccion = ref(props.ClienteExterno ? props.ClienteExterno.direccion : "");
-    const getValorRif = ref(props.ClienteExterno ? props.ClienteExterno.rif : "");
-    const getValorCodigo = ref("");
-    const getValorCantidad = ref("");
-    const getName = ref("");
-    const getDireccion = ref("");
-    const getRif = ref("");
-    const disabledInput = ref(false);
+	name: "Facturacion",
+	components: {
+		InputDinamico,
+		InputDiferente,
+		BtnGeneral,
+		ProductTable,
+	},
+	props: {
+		setListaProductosExterna: Function,
+		continuarVista: Function,
+		listaProductosInterna: Array,
+		setClienteExterno: Function,
+		ClienteExterno: Object,
+	},
+	mounted() {
+		this.cantidad = this.$refs.inputCantidad;
+		this.codigo = this.$refs.inputCodigo;
+	},
 
-    watchEffect(() => {
-      let total = 0;
-      listProductos.value.forEach((element) => {
-        total += parseFloat(element.total);
-      });
-      montoTotal.value = total.toFixed(2);
-    });
+	methods: {
+		addProduct() {
+			console.log(this.listProductos);
+			fetch("/src/json/productos.json")
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Error en la petición");
+					}
+					return response.json();
+				})
+				.then((data) => {
+					let product = data[this.codigo.inputText];
+					if (product) {
+						if (this.cantidad.inputText === "" || this.cantidad.inputText === "0") {
+							this.cantidad.inputText = "1";
+						}
 
-    const eliminarProducto = (codigo) => {
-      const index = listProductos.value.findIndex((element) => element.codigo === codigo);
-      if (index !== -1) {
-        const updatedProductos = [...listProductos.value];
-        updatedProductos.splice(index, 1);
-        listProductos.value = updatedProductos;
-      }
-    };
+						const newProduct = {
+							codigo: this.codigo.inputText,
+							descripcion: product.name,
+							cantidad: parseInt(this.cantidad.inputText),
+							precio: product.price,
+							total: parseFloat(this.cantidad.inputText) * parseFloat(product.total),
+						};
+						this.addProductToList(newProduct);
+						/* this.setListaProductosExterna(updatedProductos); */
+						this.codigo.inputText = "";
+						this.cantidad.inputText = "";
+					}
+				});
+		},
 
-    const addProduct = () => {
-      // Function logic...
-    };
+		handleOnBlur() {
+			// Function logic...
+		},
 
-    const handleOnBlur = () => {
-      // Function logic...
-    };
+		createClient() {
+			// Function logic...
+		},
 
-    const createClient = () => {
-      // Function logic...
-    };
+		continueToPayment() {
+			// Function logic...
+		},
+		eliminarProducto() {
+			// Function logic...
+		},
+	},
 
-    const continueToPayment = () => {
-      // Function logic...
-    };
+	setup(props) {
+		const listProductos = ref(props.listaProductosInterna || []);
+		const montoTotal = ref("0.00");
+		const getIdentificacion = ref("Cedula");
+		const getValorIdentificacion = ref(
+			props.ClienteExterno ? props.ClienteExterno.ci || props.ClienteExterno.pasaporte || props.ClienteExterno.idExtranjera : ""
+		);
+		const getClientes = ref([]);
+		const getValorNombre = ref(props.ClienteExterno ? props.ClienteExterno.name : "");
+		const getValorDireccion = ref(props.ClienteExterno ? props.ClienteExterno.direccion : "");
+		const getValorRif = ref(props.ClienteExterno ? props.ClienteExterno.rif : "");
+		const getValorCodigo = ref("");
+		const getValorCantidad = ref("");
+		const getName = ref("");
+		const getDireccion = ref("");
+		const getRif = ref("");
+		const disabledInput = ref(false);
 
-    return {
-      listProductos,
-      montoTotal,
-      getCantidad,
-      getCodigo,
-      getIdentificacion,
-      getValorIdentificacion,
-      getClientes,
-      getValorNombre,
-      getValorDireccion,
-      getValorRif,
-      getValorCodigo,
-      getValorCantidad,
-      getName,
-      getDireccion,
-      getRif,
-      disabledInput,
-      eliminarProducto,
-      addProduct,
-      handleOnBlur,
-      createClient,
-      continueToPayment,
-      svgAdd,
-      cartSVG,
-      svgSearch,
-    };
-  },
+		const addProductToList = (newProduct) => {
+			let productExists = false;
+			listProductos.value.forEach((product) => {
+				if (product.codigo === newProduct.codigo) {
+					product.cantidad += newProduct.cantidad;
+					product.total = product.cantidad * product.total;
+					productExists = true;
+					return;
+				}
+			});
+			if (!productExists) listProductos.value.push(newProduct);
+		};
+		const removeProduct = (codigo) => {
+			listProductos.value.forEach((product, index) => {
+				if (product.codigo === codigo) {
+					listProductos.value.splice(index, 1);
+					return;
+				}
+			});
+		};
+
+		return {
+			removeProduct,
+			listProductos,
+			addProductToList,
+			montoTotal,
+			getIdentificacion,
+			getValorIdentificacion,
+			getClientes,
+			getValorNombre,
+			getValorDireccion,
+			getValorRif,
+			getValorCodigo,
+			getValorCantidad,
+			getName,
+			getDireccion,
+			getRif,
+			disabledInput,
+			svgAdd,
+			cartSVG,
+			svgSearch,
+		};
+	},
 };
 </script>
 
 <style scoped>
 .FacturaContainer {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 85vh;
-  background-color: #ffff;
-  position: relative;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 85vh;
+	background-color: #ffff;
+	position: relative;
 }
 
 .FacturaHeaderContainer {
-  flex-basis: 10%;
-  margin-left: 7%;
-  align-items: center;
-  display: flex;
+	flex-basis: 10%;
+	margin-left: 7%;
+	align-items: center;
+	display: flex;
 }
 
 .FacturaTableContainer {
-  width: 100%;
-  display: flex;
-  align-self: center;
-  justify-content: center;
-  flex-basis: 50%;
+	width: 100%;
+	display: flex;
+	align-self: center;
+	justify-content: center;
+	flex-basis: 50%;
 }
 
 .FacturaCheckoutContainer {
-  align-self: normal;
-  flex-basis: 15%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 30px;
-  margin-left: auto;
-  margin-right: 10%;
+	align-self: normal;
+	flex-basis: 15%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 30px;
+	margin-left: auto;
+	margin-right: 10%;
 }
 
 .FacturaInputsEntre2 {
-  flex-basis: 25%;
-  display: grid;
-  grid-template-columns: 70% 30%;
+	flex-basis: 25%;
+	display: grid;
+	grid-template-columns: 70% 30%;
 }
 
 .FacturaInput1 {
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
 }
 
 .FacturaCedula-nombre {
-  align-items: center;
-  flex-basis: 33%;
-  display: flex;
-  flex-direction: row;
+	align-items: center;
+	flex-basis: 33%;
+	display: flex;
+	flex-direction: row;
 }
 
 .FacturaCedula {
-  margin-left: 10%;
-  flex-basis: 58%;
+	margin-left: 10%;
+	flex-basis: 58%;
 }
 
 .FacturaNombre {
-  flex-basis: 42%;
+	flex-basis: 42%;
 }
 
 .FacturaDireccion {
-  margin-left: 10%;
-  flex-basis: 33%;
-  width: 100%;
-  display: flex;
-  align-items: center;
+	margin-left: 10%;
+	flex-basis: 33%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 }
 
 .FacturaRif-BotonCrear {
-  align-items: center;
-  flex-basis: 33%;
-  display: flex;
-  flex-direction: row;
+	align-items: center;
+	flex-basis: 33%;
+	display: flex;
+	flex-direction: row;
 }
 
 .FacturaRif {
-  margin-left: 10%;
-  flex-basis: 60%;
+	margin-left: 10%;
+	flex-basis: 60%;
 }
 
 .FacturaBotonCrear {
-  flex-basis: 40%;
+	flex-basis: 40%;
 }
 
 .FacturaInput2 {
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 
 .FacturaoCodigo-buscar {
-  flex-basis: 33%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	flex-basis: 33%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 }
 
 .FacturaCodigo {
-  flex-basis: 50%;
+	flex-basis: 50%;
 }
 
 .FacturaBuscar {
-  flex-basis: 50%;
+	flex-basis: 50%;
 }
 
 .FacturaSearch {
-  margin-left: 20px;
-  background-color: transparent;
-  border: none;
+	margin-left: 20px;
+	background-color: transparent;
+	border: none;
 }
 
 .FacturaCantidad {
-  display: flex;
-  align-items: center;
-  flex-basis: 33%;
+	display: flex;
+	align-items: center;
+	flex-basis: 33%;
 }
 
 .FacturaBotonAgregar {
-  display: flex;
-  align-items: center;
-  flex-basis: 33%;
+	display: flex;
+	align-items: center;
+	flex-basis: 33%;
 }
 
 .hover-class:hover {
-  background-color: #c54444;
+	background-color: #c54444;
 }
 </style>
--- src/components/tables/ProductTable.vue
+++ src/components/tables/ProductTable.vue
@@ -1,136 +1,143 @@
 <template>
-    <div :style="containerStyle" class="ProductTableContainer">
-      <div class="ProductTableWrapper">
-        <div class="ProductTableHeader">
-          <p></p>
-          <p style="justify-self: left;">Código del producto</p>
-          <p>Descripción</p>
-          <p>Cantidad</p>
-          <p>Precio/Unidad</p>
-          <p>IVA</p>
-          <p>Total</p>
-          <p></p>
-        </div>
-        <div class="ProductTableBody" ref="tableBody">
-          <div v-for="(row, index) in rows" :key="index" class="ProductTableRow">
-            <button @click="eliminarProducto(row.codigo)" style="justify-self: center; border: none; background-color: transparent;">
-              <img src="../../assets/trashbin.svg" alt="Eliminar">
-            </button>
-            <p style="justify-self: left;">{{ row.codigo }}</p>
-            <p>{{ row.descripcion }}</p>
-            <p>{{ row.cantidad }}</p>
-            <p>$ {{ row.precio }}</p>
-            <p>$ {{ row.iva }}</p>
-            <p>$ {{ row.total.toFixed(2) }}</p>
-            <button style="border: none; background-color: transparent; padding-right: 15px;">
-              <img src="../../assets/explore.svg" alt="Explorar">
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ProductTable',
-    props: {
-      rows: Array
-    },
-    computed: {
-      containerStyle() {
-        return {
-          alignSelf: 'center',
-          border: '1.42917px solid #000000',
-          boxShadow: '3.81111px 5.71667px 0px #000000',
-          borderRadius: '22.8667px'
-        };
-      }
-    },
-    methods: {
-        addToCart(producto) {
-      // Agregar el producto a la lista de productos
-      this.listProductos.push(producto);
-    },
-      eliminarProducto(codigo) {
-        this.$emit('eliminarProducto', codigo);
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .ProductTableContainer {
-    align-self: center;
-    border: 1.42917px solid #000000;
-    box-sizing: border-box;
-    box-shadow: 3.81111px 5.71667px 0px #000000;
-    border-radius: 22.8667px;
-  }
-  
-  .ProductTableWrapper {
-    display: flex;
-    flex-direction: column;
-    height: 35vh;
-  }
-  
-  .ProductTableHeader {
-    display: grid;
-    align-items: center;
-    justify-items: right;
-    grid-template-columns: 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 0.5fr;
-    padding: 15px;
-    padding-right: 25px;
-    background-color: #d6d6d6;
-    border-radius: 22.8667px 22.8667px 0px 0px;
-  }
-  
-  .ProductTableBody {
-    display: flex;
-    flex-direction: column;
-    overflow-y: auto;
-    height: 100%;
-    margin-right: 10px;
-  }
-  
-  .ProductTableRow {
-    display: grid;
-    grid-template-columns: 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 0.5fr;
-    align-items: center;
-    justify-items: right;
-    margin-top: 25px;
-  }
-  
-  .ProductTableHeader p {
-    font-size: 15px;
-    font-weight: bold;
-  }
-  
-  .ProductTableRow p {
-    font-size: 15px;
-  }
-  
-  .ProductTableBody::-webkit-scrollbar {
-    width: 6px;
-    background-color: #f5f5f5;
-  }
-  
-  .ProductTableBody::-webkit-scrollbar-thumb {
-    background-color: #888;
-    border-radius: 3px;
-  }
-  
-  .ProductTableBody::-webkit-scrollbar-thumb:hover {
-    background-color: #555;
-  }
-  
-  .ProductTableBody::-webkit-scrollbar-track {
-    background-color: #dadada;
-    border-radius: 3px;
-  }
-  
-  .ProductTableBody::-webkit-scrollbar-track:hover {
-    background-color: #f1f1f1;
-  }
-  </style>
-  
+	<div :style="containerStyle" class="ProductTableContainer">
+		<div class="ProductTableWrapper">
+			<div class="ProductTableHeader">
+				<p></p>
+				<p style="justify-self: left">Código del producto</p>
+				<p>Descripción</p>
+				<p>Cantidad</p>
+				<p>Precio/Unidad</p>
+				<p>IVA</p>
+				<p>Total</p>
+				<p></p>
+			</div>
+			<div class="ProductTableBody" ref="tableBody">
+				<div v-for="(row, index) in rows" :key="index" class="ProductTableRow">
+					<button @click="eliminarProducto(row.codigo)" style="justify-self: center; border: none; background-color: transparent">
+						<img src="../../assets/trashbin.svg" alt="Eliminar" />
+					</button>
+					<p style="justify-self: left">{{ row.codigo }}</p>
+					<p>{{ row.descripcion }}</p>
+					<p>{{ row.cantidad }}</p>
+					<p>$ {{ row.precio }}</p>
+					<p>$ {{ row.iva }}</p>
+					<p>$ {{ row.total.toFixed(2) }}</p>
+					<button style="border: none; background-color: transparent; padding-right: 15px">
+						<img src="../../assets/explore.svg" alt="Explorar" />
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+	name: "ProductTable",
+	props: {
+		rows: Array,
+	},
+	computed: {
+		containerStyle() {
+			return {
+				alignSelf: "center",
+				border: "1.42917px solid #000000",
+				boxShadow: "3.81111px 5.71667px 0px #000000",
+				borderRadius: "22.8667px",
+			};
+		},
+	},
+	methods: {
+		addToCart(producto) {
+			// Agregar el producto a la lista de productos
+			this.listProductos.push(producto);
+		},
+		eliminarProducto(codigo) {
+			this.$emit("eliminarProducto", codigo);
+		},
+	},
+	setup(props) {
+		const rows = ref(props.rows);
+
+		return {
+			rows,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.ProductTableContainer {
+	align-self: center;
+	border: 1.42917px solid #000000;
+	box-sizing: border-box;
+	box-shadow: 3.81111px 5.71667px 0px #000000;
+	border-radius: 22.8667px;
+}
+
+.ProductTableWrapper {
+	display: flex;
+	flex-direction: column;
+	height: 35vh;
+}
+
+.ProductTableHeader {
+	display: grid;
+	align-items: center;
+	justify-items: right;
+	grid-template-columns: 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 0.5fr;
+	padding: 15px;
+	padding-right: 25px;
+	background-color: #d6d6d6;
+	border-radius: 22.8667px 22.8667px 0px 0px;
+}
+
+.ProductTableBody {
+	display: flex;
+	flex-direction: column;
+	overflow-y: auto;
+	height: 100%;
+	margin-right: 10px;
+}
+
+.ProductTableRow {
+	display: grid;
+	grid-template-columns: 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 0.5fr;
+	align-items: center;
+	justify-items: right;
+	margin-top: 25px;
+}
+
+.ProductTableHeader p {
+	font-size: 15px;
+	font-weight: bold;
+}
+
+.ProductTableRow p {
+	font-size: 15px;
+}
+
+.ProductTableBody::-webkit-scrollbar {
+	width: 6px;
+	background-color: #f5f5f5;
+}
+
+.ProductTableBody::-webkit-scrollbar-thumb {
+	background-color: #888;
+	border-radius: 3px;
+}
+
+.ProductTableBody::-webkit-scrollbar-thumb:hover {
+	background-color: #555;
+}
+
+.ProductTableBody::-webkit-scrollbar-track {
+	background-color: #dadada;
+	border-radius: 3px;
+}
+
+.ProductTableBody::-webkit-scrollbar-track:hover {
+	background-color: #f1f1f1;
+}
+</style>
